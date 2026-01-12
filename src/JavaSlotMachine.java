8371e63e76e0b3a42c83e3b7fda1d06c4ce09a93
Shah-zaibaly <shahxaib269@gmail.com>
2026-01-13 01:08:58 +0500

    added the payout() method that uses switch cases, each symbol carries different bet scenarios

--- src/JavaSlotMachine.java
+++ src/JavaSlotMachine.java
@@ -54,4 +54,35 @@ public class JavaSlotMachine {
     static void displayRow(String[] row){
         System.out.println(" " + String.join(" | " , row));
     }
+    static int payout(String[] row , int bet){
+        if(row[0].equals(row[1]) && row[1].equals(row[2])){
+            return switch(row[0]){
+                case "🦀" -> bet*3;
+                case "🐍" -> bet*4;
+                case "🐳" -> bet*5;
+                case "🦄" -> bet*10;
+                case "🕊️" -> bet*20;
+                default -> 0;
+            };
+        }else if(row[0].equals(row[1])) {
+            return switch (row[0]) {
+                case "🦀" -> bet * 2;
+                case "🐍" -> bet * 3;
+                case "🐳" -> bet * 4;
+                case "🦄" -> bet * 5;
+                case "🕊️" -> bet * 10;
+                default -> 0;
+            };
+        }else if(row[1].equals(row[2])) {
+            return switch (row[1]) {
+                case "🦀" -> bet * 2;
+                case "🐍" -> bet * 3;
+                case "🐳" -> bet * 4;
+                case "🦄" -> bet * 5;
+                case "🕊️" -> bet * 10;
+                default -> 0;
+            };
+        }
+        return 0;
+    }
 }
