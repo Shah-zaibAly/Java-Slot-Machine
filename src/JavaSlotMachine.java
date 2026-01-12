5274fcd141b589d77a12673894cfe26119aed679
Shah-zaibaly <shahxaib269@gmail.com>
2026-01-13 00:25:39 +0500

    added the while loop with if-else conditions for bet

--- src/JavaSlotMachine.java
+++ src/JavaSlotMachine.java
@@ -11,7 +11,26 @@ public class JavaSlotMachine {
         int payout;
         String[] symbols = {"🦀" , "🐍" , "🐳" , "🦄" , "🕊️" };
 
+        while(balance > 0) {
 
+            System.out.println("you current balance :" + balance);
+            System.out.print("Enter your bet :");
+            bet = scanner.nextInt();
+
+            if(bet > balance){
+                System.out.println("Not enough funds bud!");
+                System.out.println("Sorry for this bet.");
+            }else if(bet < 0){
+                System.out.println("The bet cannot be less than zero!");
+                System.out.println("Reconsider it.");
+            }else{
+                balance -= bet;
+            };
+
+
+
+
+        }
 
 
 
