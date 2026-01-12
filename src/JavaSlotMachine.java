831731b28e0ed9f77b1f01125e0b2d34d1724617
Shah-zaibaly <shahxaib269@gmail.com>
2026-01-13 00:18:06 +0500

    added the local variables for main

--- src/JavaSlotMachine.java
+++ src/JavaSlotMachine.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Random;
+import java.util.Scanner;
 
 public class JavaSlotMachine {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Random random  = new Random();
+
+        int balance = 1000;
+        int bet;
+        int payout;
+        String[] symbols = {"🦀" , "🐍" , "🐳" , "🦄" , "🕊️" };
+
+
+
+
+
+    }
 }
